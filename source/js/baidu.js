25baa100b8bb8d1c131ebb0ca80c99da0467bb29
edgardong <2535615874@qq.com>
2018-11-22 16:33:29 +0800

    first commit

---
+++ source/js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+var hm = document.createElement("script");
+hm.src = "https://hm.baidu.com/hm.js?893493f04974a5ef224eb3bf28405526";
+var s = document.getElementsByTagName("script")[0];
+s.parentNode.insertBefore(hm, s);
+})();
